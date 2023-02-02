49eec6a45784fb8fae18bfe5a254470603f3af60
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-02-02 17:14:14 +0100

    CORS ...

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config({ path: ".env" });
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3300;
 const { connectDB } = require("./config/DB");
@@ -8,6 +7,7 @@ const { errHandler } = require("./middleware/errormidware");
 let cors = require("cors");
 const path = require("path");
 const app = express();
+console.log(process.env.PORT);
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.use(cors());
 app.use(express.json());
